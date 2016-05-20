f5365c01d272d479da916168468e05044648cff8
Danilo Horta <danilo.horta@gmail.com>
2016-05-20 22:24:01 +0100

    crude fastlmm

--- lim/gp/test/test_fastlmm.py
+++ lim/gp/test/test_fastlmm.py
@@ -5,16 +5,16 @@ from numpy.testing import assert_almost_equal
 
 from scipy.stats import pearsonr
 
-from ..fastlmm import FastLMM
+from ..fastlmm import _FastLMMCore
 from ..regression import RegGP
 from ...util.fruits import Apples
 from ...cov import LinearCov
 from ...cov import EyeCov
 from ...cov import SumCov
 from ...mean import OffsetMean
-from ..fastlmm import FastLMM
 from ...func import check_grad
 from ...random import GPSampler
+from ...genetics import eigen_design_matrix
 
 def test_fastlmm_optimization_1():
     random = np.random.RandomState(9458)
@@ -46,8 +46,14 @@ def test_fastlmm_optimization_1():
 
     gp = RegGP(y, mean, cov)
     gp.learn()
+    delta = cov_right.scale / (cov_left.scale + cov_right.scale)
+    QS = eigen_design_matrix(X)
+    flmmc = _FastLMMCore(y, QS[0][0], QS[0][1], QS[1][0])
+    flmmc.delta = delta
+    assert_almost_equal(gp.lml(), flmmc.lml())
+    assert_almost_equal(mean.offset, flmmc.offset, decimal=6)
 
-    flmm = FastLMM(y, X)
-    flmm.learn()
-
-    assert_almost_equal(gp.lml(), flmm.lml(), decimal=5)
+    # flmmc.learn()
+    # flmm = FastLMM(y, X)
+    # flmm.learn()
+    # assert_almost_equal(gp.lml(), flmm.lml(), decimal=5)
