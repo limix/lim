ce82bb9b389d2fed69074438692e0804df1434d3
Danilo Horta <danilo.horta@gmail.com>
2016-06-05 22:43:55 +0100

    csv working well for floats

--- lim/data/csv.py
+++ lim/data/csv.py
@@ -6,7 +6,6 @@ from numpy import asarray
 
 from .slice import check_index
 from .array import ArrayViewInterface
-from .scalar import cast
 
 class CSVPath(ArrayViewInterface):
     def __init__(self, filepath, dtype=float):
@@ -14,12 +13,19 @@ class CSVPath(ArrayViewInterface):
         self._filepath = filepath
         self._dtype = dtype
 
+    def _is_col_layout(self):
+        with open(self._filepath) as csvfile:
+            row = next(csv.reader(csvfile))
+            return len(row) == 1
+
     def item(self, *args):
         if len(args) == 1:
-            r, c = args[0], 0
-        else:
-            r, c = args[0], args[1]
+            if self._is_col_layout():
+                return self._item_1d_col(args[0])
+            return self._item_1d_row(args[0])
+        return self._item_2d(args[0], args[1])
 
+    def _item_2d(self, r, c):
         i = 0
         with open(self._filepath) as csvfile:
             reader = csv.reader(csvfile)
@@ -27,7 +33,22 @@ class CSVPath(ArrayViewInterface):
             while i < r:
                 row = next(reader)
                 i += 1
-            return cast(row[c], self._dtype)
+            return self._dtype(row[c])
+
+    def _item_1d_row(self, i):
+        with open(self._filepath) as csvfile:
+            row = next(csv.reader(csvfile))
+            return self._dtype(row[i])
+
+    def _item_1d_col(self, i):
+        with open(self._filepath) as csvfile:
+            reader = csv.reader(csvfile)
+            row = next(reader)
+            j = 0
+            while j < i:
+                row = next(reader)
+                j += 1
+            return self._dtype(row[0])
 
     @property
     def dtype(self):
@@ -38,6 +59,10 @@ class CSVPath(ArrayViewInterface):
         with open(self._filepath) as csvfile:
             nc = len(next(csv.reader(csvfile)))
             nr = sum(1 for row in csv.reader(csvfile)) + 1
+        if nr == 1:
+            return (nc,)
+        if nc == 1:
+            return (nr,)
         return nr, nc
 
     @property
@@ -47,7 +72,9 @@ class CSVPath(ArrayViewInterface):
     def _create_array(self, lslice):
         if self.ndim == 1:
             assert len(lslice) == 1
-            return self._create_1d_array(lslice)
+            if self._is_col_layout():
+                return self._create_1d_array_col(lslice[0])
+            return self._create_1d_array_row(lslice[0])
         if self.ndim == 2:
             assert len(lslice) == 2
             return self._create_2d_array(lslice[0], lslice[1])
@@ -56,8 +83,22 @@ class CSVPath(ArrayViewInterface):
     def __array__(self):
         return self._create_array((slice(0, None, 1),)*self.ndim)
 
-    def _create_1d_array(self, slice_):
-        pass
+    def _create_1d_array_row(self, slice_):
+        with open(self._filepath) as csvfile:
+            reader = csv.reader(csvfile)
+            row = next(reader)
+            n = len(row)
+            srow = [row[j] for j in range(n) if check_index(j, slice_) == 1]
+            return asarray(srow).astype(self._dtype)
+
+    def _create_1d_array_col(self, slice_):
+        with open(self._filepath) as csvfile:
+            reader = csv.reader(csvfile)
+            srows = []
+            for (i, row) in enumerate(reader):
+                if check_index(i, slice_) == 1:
+                    srows.append(row[0])
+            return asarray(srows).astype(self._dtype)
 
     def _create_2d_array(self, row_slice, col_slice):
         rs = row_slice
--- lim/test/test_array_readers.py
+++ lim/test/test_array_readers.py
@@ -7,6 +7,22 @@ from numpy.testing import assert_equal
 
 import lim
 
+def _test_1d(reader, fp, dtype):
+    R = loadtxt(fp, delimiter=',', dtype=dtype)
+    X = reader(fp, dtype=dtype)
+    assert_array_equal(X, R)
+    assert_array_equal(X[1:], R[1:])
+    assert_array_equal(X[1:-1], R[1:-1])
+    assert_array_equal(X[-2:-1], R[-2:-1])
+    assert_equal(X.shape, R.shape)
+    assert_equal(X.ndim, R.ndim)
+    for i in range(X.shape[0]):
+        assert_equal(X[i], R[i])
+
+    assert_equal(bytes(X), bytes(R))
+
+    assert_array_equal(asarray(X[:]), R)
+
 def _test_2d(reader, fp, dtype):
     R = loadtxt(fp, delimiter=',', dtype=dtype)
     X = reader(fp, dtype=dtype)
@@ -26,11 +42,12 @@ def _test_2d(reader, fp, dtype):
 
     assert_array_equal(asarray(X[:]), R)
 
-
 def test_2d():
     root = os.path.dirname(os.path.realpath(__file__))
     root = os.path.join(root, 'data')
+
+    _test_1d(lim.csvpath, os.path.join(root, '1d_array_col.csv'), float)
+    _test_1d(lim.csvpath, os.path.join(root, '1d_array_row.csv'), float)
+
     _test_2d(lim.csvpath, os.path.join(root, '2d_array.csv'), float)
     _test_2d(lim.csvpath, os.path.join(root, '2d_array_bytes.csv'), bytes)
-
-test_2d()
