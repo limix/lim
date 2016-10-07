b18209b40b2f920ec0a798ecdae2422555dd56c4
Danilo Horta <danilo.horta@gmail.com>
2016-10-07 17:14:50 +0100

    GLMMSampler

---
+++ lim/random/glmm.py
@@ -0,0 +1,21 @@
+from __future__ import division
+
+from numpy.random import RandomState
+
+
+class GLMMSampler(object):
+
+    def __init__(self, lik, mean, cov):
+        self._lik = lik
+        self._mean = mean
+        self._cov = cov
+
+    def sample(self, random_state=None):
+        if random_state is None:
+            random_state = RandomState()
+
+        m = self._mean.data('sample').value()
+        K = self._cov.data('sample').value()
+        u = random_state.multivariate_normal(m, K)
+
+        return self._lik.sample(u)
