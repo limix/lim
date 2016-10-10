64161a8eacb1697793af9cec77bc94b6e336322e
Danilo Horta <danilo.horta@gmail.com>
2016-10-10 13:47:56 +0100

    require optimix>=1.0

--- setup.py
+++ setup.py
@@ -20,7 +20,8 @@ def setup_package():
                         'limix_math>=0.3', 'bidict', 'progressbar2>=3.10',
                         'h5py>=2.6', 'pandas>=0.18', 'nose>=1.3',
                         'tabulate>=0.7', 'six',
-                        'ndarray_listener>=1.0']
+                        'ndarray_listener>=1.0',
+                        'optimix>=1.0']
     tests_require = install_requires
 
     metadata = dict(
