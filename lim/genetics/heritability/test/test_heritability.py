a3f5a1f4287b41036acb9079d499435bfd1dd690
Danilo Horta <danilo.horta@gmail.com>
2016-11-07 18:08:39 +0000

    assert_allclose(bernoulli_estimate(y, X), 3.705859167105969e-07, atol=1e-4)

--- lim/genetics/heritability/test/test_heritability.py
+++ lim/genetics/heritability/test/test_heritability.py
@@ -14,8 +14,9 @@ def test_heritability_bernoulli_estimate():
     random = RandomState(1)
     N = 50
     X = random.randn(N, N+1)
-    y = bernoulli_sampler(0.1, X, random_state=random)
-    assert_allclose(bernoulli_estimate(y, X), 0.627441483331, rtol=1e-5)
+    y = bernoulli_sampler(0.0, X, random_state=random)
+    # assert_allclose(bernoulli_estimate(y, X), 0.627441483331, rtol=1e-5)
+    assert_allclose(bernoulli_estimate(y, X), 3.705859167105969e-07, atol=1e-4)
 
 
 def test_heritability_binomial_estimate():
