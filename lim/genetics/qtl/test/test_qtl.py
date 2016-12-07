99de95e67465dfe3cd9123464937964d893c7aff
Danilo Horta <danilo.horta@gmail.com>
2016-12-07 12:59:48 +0000

    binomial qtl back

--- lim/genetics/qtl/test/test_qtl.py
+++ lim/genetics/qtl/test/test_qtl.py
@@ -33,96 +33,94 @@ def test_qtl_normal_scan():
     y = dot(G, u1) + dot(X, u2)
 
     qtl = scan(NormalPhenotype(y), X, G=G, progress=False)
-    for p in qtl.pvalues():
-        print(p)
-    # assert_allclose(
-    #     qtl.pvalues(), [
-    #         0.0705409218415, 1.33152093178e-13, 2.48707231047e-05,
-    #         0.259664271433, 0.93041182258
-    #     ],
-    #     rtol=1e-5)
+    assert_allclose(
+        qtl.pvalues(), [
+            0.0705409218415, 1.33152093178e-13, 2.48707231047e-05,
+            0.259664271433, 0.93041182258
+        ],
+        rtol=1e-5)
 
 
-# def test_qtl_normal_scan_covariate_redundance():
-#     random = RandomState(2)
-#
-#     N = 50
-#     G = random.randn(N, N + 100)
-#     G = stdnorm(G, 0)
-#     G /= sqrt(G.shape[1])
-#
-#     p = 5
-#     X = random.randn(N, p)
-#     X = stdnorm(X, 0)
-#     X /= sqrt(X.shape[1])
-#
-#     u1 = random.randn(N+100) / sqrt(N+100)
-#     u2 = random.randn(p) / sqrt(p)
-#
-#     y = dot(G, u1) + dot(X, u2)
-#
-#     X[:] = 1
-#     qtl = scan(NormalPhenotype(y), X, G=G, progress=False)
-#     assert_allclose(qtl.pvalues(), [1]*p)
-#
-# def test_qtl_binomial_scan():
-#     random = RandomState(9)
-#
-#     N = 50
-#     G = random.randn(N, N + 100)
-#     G = stdnorm(G, 0)
-#     G /= sqrt(G.shape[1])
-#
-#     p = 5
-#     X = random.randn(N, p)
-#     X = stdnorm(X, 0)
-#     X /= sqrt(X.shape[1])
-#
-#     ntrials = random.randint(1, 50, N)
-#     nsuccesses = binomial(
-#         ntrials,
-#         -0.1,
-#         G,
-#         causal_variants=X,
-#         causal_variance=0.1,
-#         random_state=random)
-#
-#     qtl = scan(BinomialPhenotype(nsuccesses, ntrials), X, G=G, progress=False)
-#     assert_allclose(
-#         qtl.pvalues(), [
-#             0.0275759504728,
-#             0.570733489676,
-#             0.69020414305,
-#             0.757099966035,
-#             0.0937948997269,
-#         ],
-#         rtol=1e-5)
-#
-# def test_qtl_binomial_scan_covariate_redundance():
-#     random = RandomState(9)
-#
-#     N = 50
-#     G = random.randn(N, N + 100)
-#     G = stdnorm(G, 0)
-#     G /= sqrt(G.shape[1])
-#
-#     p = 5
-#     X = random.randn(N, p)
-#     X = stdnorm(X, 0)
-#     X /= sqrt(X.shape[1])
-#
-#     ntrials = random.randint(1, 50, N)
-#     nsuccesses = binomial(
-#         ntrials,
-#         -0.1,
-#         G,
-#         causal_variants=X,
-#         causal_variance=0.1,
-#         random_state=random)
-#
-#     X[:] = 1
-#     qtl = scan(BinomialPhenotype(nsuccesses, ntrials), X, G=G, progress=False)
-#     assert_allclose(qtl.pvalues(), [1] * p)
+def test_qtl_normal_scan_covariate_redundance():
+    random = RandomState(2)
+
+    N = 50
+    G = random.randn(N, N + 100)
+    G = stdnorm(G, 0)
+    G /= sqrt(G.shape[1])
+
+    p = 5
+    X = random.randn(N, p)
+    X = stdnorm(X, 0)
+    X /= sqrt(X.shape[1])
+
+    u1 = random.randn(N+100) / sqrt(N+100)
+    u2 = random.randn(p) / sqrt(p)
+
+    y = dot(G, u1) + dot(X, u2)
+
+    X[:] = 1
+    qtl = scan(NormalPhenotype(y), X, G=G, progress=False)
+    assert_allclose(qtl.pvalues(), [1]*p)
+
+def test_qtl_binomial_scan():
+    random = RandomState(9)
+
+    N = 50
+    G = random.randn(N, N + 100)
+    G = stdnorm(G, 0)
+    G /= sqrt(G.shape[1])
+
+    p = 5
+    X = random.randn(N, p)
+    X = stdnorm(X, 0)
+    X /= sqrt(X.shape[1])
+
+    ntrials = random.randint(1, 50, N)
+    nsuccesses = binomial(
+        ntrials,
+        -0.1,
+        G,
+        causal_variants=X,
+        causal_variance=0.1,
+        random_state=random)
+
+    qtl = scan(BinomialPhenotype(nsuccesses, ntrials), X, G=G, progress=False)
+    assert_allclose(
+        qtl.pvalues(), [
+            0.0275759504728,
+            0.570733489676,
+            0.69020414305,
+            0.757099966035,
+            0.0937948997269,
+        ],
+        rtol=1e-2)
+
+def test_qtl_binomial_scan_covariate_redundance():
+    random = RandomState(9)
+
+    N = 50
+    G = random.randn(N, N + 100)
+    G = stdnorm(G, 0)
+    G /= sqrt(G.shape[1])
+
+    p = 5
+    X = random.randn(N, p)
+    X = stdnorm(X, 0)
+    X /= sqrt(X.shape[1])
+
+    ntrials = random.randint(1, 50, N)
+    nsuccesses = binomial(
+        ntrials,
+        -0.1,
+        G,
+        causal_variants=X,
+        causal_variance=0.1,
+        random_state=random)
+
+    X[:] = 1
+    qtl = scan(BinomialPhenotype(nsuccesses, ntrials), X, G=G, progress=False)
+    assert_allclose(qtl.pvalues(), [1] * p)
 #
 # def test_qtl_poisson_scan():
 #     random = RandomState(9)
--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ def setup_package():
 
     metadata = dict(
         name='lim',
-        version='1.2.0.dev3',
+        version='1.2.0.dev4',
         maintainer="Limix Developers",
         maintainer_email="horta@ebi.ac.uk",
         license="MIT",
